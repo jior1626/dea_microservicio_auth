fe8db98145bd7850665b0cf9a78147ef9c631c3d
inteligenciafuturatv@gmail.com <inteligenciafuturatv@gmail.com>
2023-11-13 18:30:33 -0500

    cors

--- api.php
+++ api.php
@@ -1,13 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 // api.php
 include_once 'Database.php';
 include_once 'Usuario.php';
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+/*header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");*/
 
 $database = new Database();
 $db = $database->getConnection();
